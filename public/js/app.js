6edb28e102bfdf5bac80c1a165bf4ce1c6fa8cd8
bwong199 <bwong199@my.bcit.ca>
2016-06-18 16:33:34 -0600

    Finished assignment except build process

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var dashboardApp = angular.module('dashboardApp', ['ngRoute', 'ngResource', 'zingchart-angularjs']);
+var dashboardApp = angular.module('dashboardApp', ['ngRoute', 'ngResource', 'zingchart-angularjs', 'uiGmapgoogle-maps']);
 
 
 
@@ -19,46 +19,8 @@ dashboardApp.controller('issuesController', ['$scope',  '$resource', function($s
 	console.log($scope.issuesResult);
 }])
 
-var cities = [
-    {
-        city : 'Toronto',
-        desc : 'This is the best city in the world!',
-        lat : 43.7000,
-        long : -79.4000
-    },
-    {
-        city : 'New York',
-        desc : 'This city is aiiiiite!',
-        lat : 40.6700,
-        long : -73.9400
-    },
-    {
-        city : 'Chicago',
-        desc : 'This is the second best city in the world!',
-        lat : 41.8819,
-        long : -87.6278
-    },
-    {
-        city : 'Los Angeles',
-        desc : 'This city is live!',
-        lat : 34.0500,
-        long : -118.2500
-    },
-    {
-        city : 'Las Vegas',
-        desc : 'Sin City...\'nuff said!',
-        lat : 36.0800,
-        long : -115.1522
-    }
-];
 
 
-dashboardApp.controller('employeesController', ['$scope',  '$resource', '$window', function($scope, $resource, $window){
-
-
-
-
-}])
 
 
 
--- public/js/keyMetricsController.js
+++ public/js/keyMetricsController.js
@@ -11,20 +11,23 @@ dashboardApp.controller('keyMetricsController',  [ '$scope', '$resource', '$rout
 
 	var array = [];
 
-	var customerArray = [];
-	var dateArray = [];
+ $scope.customerArray = [];
+ $scope.dateArray = [];
+ $scope.reportedIssues = [];
+
+  $scope.response = '';
 
     $http.get("https://corporate-dashboard.firebaseio.com/key_metrics.json")
     .then(function(response) {
 
-        $scope.myJson.series[0].values = $scope.myJson.series[0].values.concat(response.data.values);
-
-        console.log(response);
-
         for (x in response.data){
-        	customerArray.push(response.data[x].number_of_paying_customers)
-        	dateArray.push(response.data[x].date);
+
+        	$scope.customerArray.push(response.data[x].number_of_paying_customers)
+        	$scope.dateArray.push(response.data[x].date);
+          $scope.reportedIssues.push(response.data[x].reported_issues);
         }
+
+        console.log($scope.customerArray)
     });
 
   $scope.myJson = {
@@ -92,7 +95,7 @@ dashboardApp.controller('keyMetricsController',  [ '$scope', '$resource', '$rout
       tick : {
         visible : false
       },
-      labels : dateArray
+      labels : $scope.dateArray
     },
     scaleY : {
       lineColor : "#E3E8E9",
@@ -109,7 +112,7 @@ dashboardApp.controller('keyMetricsController',  [ '$scope', '$resource', '$rout
     },
     series : [
         {
-            values : customerArray,
+            values : $scope.customerArray,
             text : "Paying Customers",
             lineColor : "#00ACF2",
             marker : {
@@ -118,4 +121,97 @@ dashboardApp.controller('keyMetricsController',  [ '$scope', '$resource', '$rout
         }
     ]
 };
+
+  $scope.myJson2 = {
+    type: "bar", 
+    title: {
+      textAlign:'left',
+    },
+    crosshairX : {
+      lineColor : "#b6b6b6",
+      trigger : "move",
+      lineStyle : 'dashed',
+      marker : {
+        visible : true,
+      size : 4
+      },
+      scaleLabel : {
+        bold : true,
+        backgroundColor : "#fff",
+        fontColor : "#474747",
+        fontSize : "16px",
+        callout : false,
+        paddingTop : 2,
+        
+      },
+      plotLabel : {
+        backgroundColor : "white",
+        borderColor : "#bababa",
+        borderRadius : "5px",
+        bold : true,
+        fontSize : "12px",
+        fontColor : "#2f2f2f",
+        textAlign : 'right',
+        padding : '10px',
+        shadow : true,
+        shadowAlpha : 0.2,
+        shadowBlur : 5,
+        shadowDistance : 4,
+        shadowColor : "#a1a1a1",
+        
+      }
+    },
+    plot : {
+      tooltip : {
+        visible : false
+      },
+      aspect : 'spline',
+      marker : {
+        backgroundColor : "white",
+        borderWidth : "2px",
+      },
+      hoverMarker : {
+        backgroundColor : 'none',
+        size : 10
+      }
+    },
+    scaleX : {
+      lineColor : "#E3E8E9",
+      fontColor : "#879CAB",
+      guide :{
+        visible : true,
+        lineWidth : "1px",
+        lineColor : "#E3E8E9",
+        lineStyle : "solid"
+      },
+      tick : {
+        visible : false
+      },
+      labels : $scope.dateArray
+    },
+    scaleY : {
+      lineColor : "#E3E8E9",
+      fontColor : "#879CAB",
+      guide :{
+        visible : true,
+        lineWidth : "1px",
+        lineColor : "#E3E8E9",
+        lineStyle : "solid"
+      },
+       tick : {
+        visible : false
+      }
+    },
+    series : [
+        {
+            values : $scope.reportedIssues,
+            text : "Paying Customers",
+            lineColor : "#00ACF2",
+            marker : {
+        borderColor : "#00ACF2"
+      }
+        }
+    ]
+};
+
 }])
